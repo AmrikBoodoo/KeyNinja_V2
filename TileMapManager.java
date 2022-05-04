28a4c7b33f28e85e24e5ccd7b7ab43c40b2d53ed
AmrikBoodoo <boodooamrik@gmail.com>
2022-05-04 19:49:52 -0400

    2nd commit

--- TileMapManager.java
+++ TileMapManager.java
@@ -7,11 +7,7 @@ import javax.swing.ImageIcon;
 
 
 
-/**
-    The ResourceManager class loads and manages tile Images and
-    "host" Sprites used in the game. Game Sprites are cloned from
-    "host" Sprites.
-*/
+
 public class TileMapManager {
 
     private ArrayList<Image> tiles;
@@ -19,18 +15,6 @@ public class TileMapManager {
 
     private JFrame window;
 
-
-/*
-    private GraphicsConfiguration gc;
-
-    // host sprites used for cloning
-    private Sprite playerSprite;
-    private Sprite musicSprite;
-    private Sprite coinSprite;
-    private Sprite goalSprite;
-    private Sprite grubSprite;
-    private Sprite flySprite;
-*/
 
     public TileMapManager(JFrame window) {
 	this.window = window;
@@ -137,68 +121,14 @@ public class TileMapManager {
                     
                     
                 }
-/*
-                // check if the char represents a sprite
-                else if (ch == 'o') {
-                    addSprite(newMap, coinSprite, x, y);
-                }
-                else if (ch == '!') {
-                    addSprite(newMap, musicSprite, x, y);
-                }
-                else if (ch == '*') {
-                    addSprite(newMap, goalSprite, x, y);
-                }
-                else if (ch == '1') {
-                    addSprite(newMap, grubSprite, x, y);
-                }
-                else if (ch == '2') {
-                    addSprite(newMap, flySprite, x, y);
-                }
-*/
+
             }
         }
 
-/*
-        // add the player to the map
-        Sprite player = (Sprite)playerSprite.clone();
-        player.setX(TileMapRenderer.tilesToPixels(3));
-
-        player.setY(0);
-        newMap.setPlayer(player);
-*/      
         return newMap;
     }
 
 
-/*
-    private void addSprite(TileMap map,
-        Sprite hostSprite, int tileX, int tileY)
-    {
-        if (hostSprite != null) {
-            // clone the sprite from the "host"
-            Sprite sprite = (Sprite)hostSprite.clone();
-
-            // center the sprite
-            sprite.setX(
-                TileMapRenderer.tilesToPixels(tileX) +
-                (TileMapRenderer.tilesToPixels(1) -
-                sprite.getWidth()) / 2);
-
-            // bottom-justify the sprite
-            sprite.setY(
-                TileMapRenderer.tilesToPixels(tileY + 1) -
-                sprite.getHeight());
-
-            // add it to the map
-            map.addSprite(sprite);
-        }
-    }
-
-*/
-
-    // -----------------------------------------------------------
-    // code for loading sprites and images
-    // -----------------------------------------------------------
 
 
     public void loadTileImages() {
@@ -226,59 +156,6 @@ public class TileMapManager {
         }
     }
 
-/*
-    public void loadCreatureSprites() {
-
-        Image[][] images = new Image[4][];
-
-        // load left-facing images
-        images[0] = new Image[] {
-            loadImage("player1.png"),
-            loadImage("player2.png"),
-            loadImage("player3.png"),
-            loadImage("fly1.png"),
-            loadImage("fly2.png"),
-            loadImage("fly3.png"),
-            loadImage("grub1.png"),
-            loadImage("grub2.png"),
-        };
-
-        images[1] = new Image[images[0].length];
-        images[2] = new Image[images[0].length];
-        images[3] = new Image[images[0].length];
-        for (int i=0; i<images[0].length; i++) {
-            // right-facing images
-            images[1][i] = getMirrorImage(images[0][i]);
-            // left-facing "dead" images
-            images[2][i] = getFlippedImage(images[0][i]);
-            // right-facing "dead" images
-            images[3][i] = getFlippedImage(images[1][i]);
-        }
-
-        // create creature animations
-        Animation[] playerAnim = new Animation[4];
-        Animation[] flyAnim = new Animation[4];
-        Animation[] grubAnim = new Animation[4];
-        for (int i=0; i<4; i++) {
-            playerAnim[i] = createPlayerAnim(
-                images[i][0], images[i][1], images[i][2]);
-            flyAnim[i] = createFlyAnim(
-                images[i][3], images[i][4], images[i][5]);
-            grubAnim[i] = createGrubAnim(
-                images[i][6], images[i][7]);
-        }
-
-        // create creature sprites
-        playerSprite = new Player(playerAnim[0], playerAnim[1],
-            playerAnim[2], playerAnim[3]);
-        flySprite = new Fly(flyAnim[0], flyAnim[1],
-            flyAnim[2], flyAnim[3]);
-        grubSprite = new Grub(grubAnim[0], grubAnim[1],
-            grubAnim[2], grubAnim[3]);
-System.out.println("loadCreatureSprites successfully executed.");
-
-    }
-*/
 
 
 }
